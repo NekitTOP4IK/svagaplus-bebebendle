1bcf4bfc4e64733f05d1e60de4654d7814378705
NekitTOP4IK <nekittop4ik2@gmail.com>
2026-07-24 12:45:37 +0300

    style: restore pixel font on home user menu nick
    HomeUserMenu UserIdentity uses pixelFont so the current-user chip matches the rest of the UI.

--- next/components/home-user-menu.tsx
+++ next/components/home-user-menu.tsx
@@ -116,6 +116,7 @@ export function HomeUserMenu(): ReactElement {
           meta="Профиль / СВАГА+"
           showMetaSuffix={false}
           nickGlow
+          pixelFont
         />
       </Link>
 
